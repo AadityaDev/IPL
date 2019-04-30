a0d6039898f6790e883936723712e9c1cfe4d3f9
Manish Kumar <manish.kumar11@genpact.com>
2019-04-30 22:17:42 +0530

    imaegs added

--- src/views/dashboard/Home.vue
+++ src/views/dashboard/Home.vue
@@ -34,10 +34,11 @@
                   <h3>{{team.Team_Short_Code}}</h3>
                 </div>
                 <div slot="media">
-                  <img alt="Vue logo" src="../../assets/logo.png">
+                  <!-- <img :src="team.img" /> -->
+                  <img src='team.img' />
                 </div>
                 <div>
-                  <span>Hi {{team.Team_Name}}</span>
+                  <span>{{team.Team_Name}}</span>
                 </div>
                 <div slot="footer">
                   <vs-row vs-justify="flex-end">
@@ -86,8 +87,37 @@ export default {
       console.log('readMatchesInfo is',matchesResponse.length);
       console.log('matches response: ', matchesResponse);
       for(let index = 0; index<matchesResponse.length; index++){
-        console.log(JSON.stringify(matchesResponse[index]));
+
+        switch(matchesResponse[index][Match_Id]){
+          case 1: matchesResponse[index]['img'] = '../../assets/imgs/teams/kolkata-knight-riders.jpg';
+          break;
+          case 2: matchesResponse[index]['img'] = '../../assets/imgs/teams/rc-bangalore.jpg';
+          break;
+          case 3: matchesResponse[index]['img'] = '../../assets/imgs/teams/chennai-super-kings.jpg';
+          break;
+          case 4: matchesResponse[index]['img'] = '../../assets/imgs/teams/kings-eleven-punjab.jpg';
+          break;
+          case 5: matchesResponse[index]['img'] = '../../assets/imgs/teams/rajasthan-royals.jpg';
+          break;
+          case 6: matchesResponse[index]['img'] = '../../assets/imgs/teams/delhi-capitals.jpg';
+          break;
+          case 7: matchesResponse[index]['img'] = '../../assets/imgs/teams/mumbai-indians.jpg';
+          break;
+          case 8: matchesResponse[index]['img'] = '../../assets/imgs/teams/rajasthan-royals.jpg';
+          break;
+          case 9: matchesResponse[index]['img'] = '../../assets/imgs/teams/rajasthan-royals.jpg';
+          break;
+          case 10: matchesResponse[index]['img'] = '../../assets/imgs/teams/rajasthan-royals.jpg';
+          break;
+          case 11: matchesResponse[index]['img'] = '../../assets/imgs/teams/rajasthan-royals.jpg';
+          break;
+          case 12: matchesResponse[index]['img'] = '../../assets/imgs/teams/rajasthan-royals.jpg';
+          break;
+          case 13: matchesResponse[index].img = '../../assets/imgs/teams/rajasthan-royals.jpg';
+          break;
+        }
         this.matches.push(matchesResponse[index]);
+        this.matches[index].img = '../../assets/imgs/teams/rajasthan-royals.jpg';
         console.log(JSON.stringify(this.matches[index]));
       }
     },
